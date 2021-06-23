6fab35c3af2a59ca1d52b562f3948378e1281e4f
BRgis <giseleoliveira20@gmail.com>
2021-06-23 13:51:43 -0300

    Adicionando aqrquivos do exercício 1

---
+++ Dia 1/exercicio_dia1.js
@@ -0,0 +1,42 @@
+//1. Escreva um comentário de uma única linha que diga: os comentários podem tornar o código legível
+// Início do exercício
+
+//2.Escreva outro único comentário que diga: 
+//Bem-vindo a 30DaysOfJavaScript*/
+
+/*3.Escreva um comentário em várias linhas que diga: 
+  Os comentários podem tornar o código legível, fácil de reutilizar e informativo.*/
+  
+  //6.Declare quatro variáveis ​​sem atribuir valores
+let var_um
+let var_dois
+let var_tres
+let var_quatro
+
+//7.Declare quatro variáveis ​​com valores atribuídos
+let var_cinco = 5
+let var_seis = 6
+let var_sete = 7 
+let var_oito = 8 
+
+//8. Declare variáveis ​​para armazenar seu nome, sobrenome, estado civil, cidade e idade em várias linhas
+let nome = 'Gisele'
+let sobrenome = 'Oliveira'
+let estado_civil = 'Casada'
+let cidade = 'Florianópolis'
+let idade = 38
+
+//9. Declare variáveis ​​para armazenar seu nome, sobrenome, estado civil, país e idade em uma única linha
+let nome_ = 'Gisele',
+    sobrenome_ = 'Oliveira',
+    estado_civil_ = 'Casada',
+    cidade_ = 'Florianópolis',
+    idade_ = 38
+
+//10.Declare duas variáveis myAge e yourAge e atribua a elas valores iniciais e registre no console do navegador.
+
+let myAge = 38
+let yourAge = 25
+
+console.log('Eu tenho ',myAge,' anos de idade.')
+console.log('Você tem ',yourAge,' anos.')
